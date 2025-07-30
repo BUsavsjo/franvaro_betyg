f49bc4208227432d13258802f1cefd244a2db49b
Peter Wenström <peter.wenstrom@savsjo.se>
2025-07-30 15:07:37 +0200

    la till mappar i data för import

---
+++ src/busavsjo_samla_betygstxt.py
@@ -0,0 +1,29 @@
+import os
+
+def busavsjo_samla_txtfiler():
+    """
+    Läser in alla .txt-filer i data/import data betyg och sparar ihop dem till data/betyg.txt
+    """
+    rotmapp = os.path.dirname(os.path.dirname(__file__))
+    indata_mapp = os.path.join(rotmapp, "data", "import data betyg")
+    utdata_fil = os.path.join(rotmapp, "data", "betyg.txt")
+    med_antal = 0
+
+    with open(utdata_fil, "w", encoding="utf-8") as utfil:
+        for filnamn in sorted(os.listdir(indata_mapp)):
+            if filnamn.endswith(".txt") and filnamn != "betyg.txt":
+                filväg = os.path.join(indata_mapp, filnamn)
+                try:
+                    with open(filväg, "r", encoding="utf-8") as infil:
+                        innehåll = infil.read()
+                except UnicodeDecodeError:
+                    with open(filväg, "r", encoding="latin-1") as infil:
+                        innehåll = infil.read()
+
+                utfil.write(innehåll.strip() + "\n\n")
+                med_antal += 1
+
+    print(f"✔️ Samlade {med_antal} filer till '{utdata_fil}'")
+
+if __name__ == "__main__":
+    busavsjo_samla_txtfiler()
